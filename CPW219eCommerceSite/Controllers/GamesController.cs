b6954caf10c241a678f8931c9b8b4fc5a078ca0b
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-06 14:59:26 -0800

    Made a Create Game view

---
+++ CPW219eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
