767e73b1862839a8088022581bb9bd5b678336b0
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-06 15:21:50 -0800

    Create Game fully functional. Added some styling.

--- CPW219eCommerceSite/Controllers/GamesController.cs
+++ CPW219eCommerceSite/Controllers/GamesController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219eCommerceSite.Data;
+using CPW219eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace CPW219eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly VideoGameContext _context;
+
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Games.Add(game);   // Prepares insert
+                _context.SaveChanges();     // Executes pending insert
+
+                ViewData["Message"] = $"{game.Title} was added successfully";
+                return View();
+            }
+            return View(game);
         }
     }
 }
