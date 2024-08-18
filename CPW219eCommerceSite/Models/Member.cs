af2d6c39c0ced1f7c377d3ecca7fc7701efac353
eskelProgramming <eskelley0673@students.cptc.edu>
2024-08-18 15:28:59 -0700

    Made Member Phone and Username optional

--- CPW219eCommerceSite/Models/Member.cs
+++ CPW219eCommerceSite/Models/Member.cs
@@ -8,13 +8,13 @@ namespace CPW219eCommerceSite.Models
         [Key]
         public int MemberId { get; set; }
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
-        public string Username { get; set; }
+        public string? Username { get; set; }
     }
 
     [Keyless]
