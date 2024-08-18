daee88b94304f9410a31ce3396f00789c103060f
eskelProgramming <eskelley0673@students.cptc.edu>
2024-08-18 16:24:37 -0700

    Refactored Game Catalog to use ViewModel

---
+++ CPW219eCommerceSite/Models/GameCatalogViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CPW219eCommerceSite.Models
+{
+    public class GameCatalogViewModel
+    {
+        public GameCatalogViewModel(List<Game> games, int lastPage, int currentPage)
+        {
+            Games = games;
+            LastPage = lastPage;
+            CurrentPage = currentPage;
+        }
+
+        public List<Game> Games { get; private set; }
+
+        /// <summary>
+        /// The last page of the catalog. Calculated by
+        /// having a total number of products divided by
+        /// products per page. 
+        /// </summary>
+        public int LastPage { get; private set; }
+
+        public int CurrentPage { get; private set; }
+    }
+}
