676d181d2a5db975d5a37c46f9653d8dff09f0d8
eskelProgramming <eskelley0673@students.cptc.edu>
2024-08-23 17:47:50 -0700

    Coded Add to cart, Remove from cart, and added a cart summary page

--- CPW219eCommerceSite/Controllers/CartController.cs
+++ CPW219eCommerceSite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using CPW219eCommerceSite.Data;
 using CPW219eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CPW219eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
         private readonly VideoGameContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(VideoGameContext context)
         {
@@ -24,10 +26,67 @@ namespace CPW219eCommerceSite.Controllers
                 return RedirectToAction("Index", "Games");
             }
 
+            CartGameViewModel cartGame = new()
+            {
+                GameId = gameToAdd.GameId,
+                Title = gameToAdd.Title,
+                Price = gameToAdd.Price
+            };
+
+            List<CartGameViewModel> cartGames = GetExistingCartData();
+            cartGames.Add(cartGame);
+            WriteShoppingCartCookie(cartGames);
             // TOdo: Add game to a shopping cart cookie
 
             TempData["Message"] = $"{gameToAdd.Title} added to cart";
             return RedirectToAction("Index", "Games");
+        }
+
+        private void WriteShoppingCartCookie(List<CartGameViewModel> cartGames)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartGames);
+
+            // Serialization JSON
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+        }
+
+        /// <summary>
+        /// Return the current list of video games in the users shopping 
+        /// cart cookie. If there is no cookie, an empty list will be returned.
+        /// </summary>
+        /// <returns></returns>
+        private List<CartGameViewModel> GetExistingCartData()
+        {
+            string cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartGameViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartGameViewModel>>(cookie);
+        }
+
+        public IActionResult Summary()
+        {
+            // Read shopping cart data and convert to list of view model
+            List<CartGameViewModel> cartGames = GetExistingCartData();
+            return View(cartGames);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartGameViewModel> cartGames = GetExistingCartData();
+
+            CartGameViewModel? gameToRemove = cartGames.Where(g => g.GameId == id).FirstOrDefault();
+
+            cartGames.Remove(gameToRemove);
+            
+            WriteShoppingCartCookie(cartGames);
+
+            return RedirectToAction("Summary");
         }
     }
 }
